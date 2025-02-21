1a14d69c69d26a57378444c589df17971299bd5b
proSamik <samikchoudhury15@gmail.com>
2025-02-21 22:23:01 +0530

    Add margin-top to footer for improved page layout

--- client/src/components/landing/Footer.tsx
+++ client/src/components/landing/Footer.tsx
@@ -42,7 +42,7 @@ const navigation = {
 
 export function Footer() {
   return (
-    <footer className="bg-gray-900" aria-labelledby="footer-heading">
+    <footer className="bg-gray-900 mt-auto" aria-labelledby="footer-heading">
       <h2 id="footer-heading" className="sr-only">
         Footer
       </h2>
